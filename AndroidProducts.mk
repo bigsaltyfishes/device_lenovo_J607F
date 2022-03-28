dc28a618af31addba2c1aa0a0396a571535b5583
bigsaltyfishes <32119500038@e.gzhu.edu.cn>
2022-03-28 23:45:46 +0800

    J607F: Initial device specific tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_J607F.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_gauguin-userdebug \
+    aosp_gauguin-eng
