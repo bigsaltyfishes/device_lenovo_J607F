59b6acd7bc9a54a4f29ac921c1bf67f767960da7
bigsaltyfishes <32119500038@e.gzhu.edu.cn>
2022-04-02 23:30:56 +0800

    Import aidl QTI vibrator HAL from LA.UM.9.12.r1-11300-SMxx50.0

--- device.mk
+++ device.mk
@@ -431,7 +431,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.lenovo_arnoz
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/etc/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
