32ab4131fa45bc6c6d9e19ee443b2aa27792a1b3
A Big Salty Fish <1055126159@qq.com>
2022-05-07 18:20:50 +0800

    J607F: Drop IFAA Permissions

--- device.mk
+++ device.mk
@@ -330,9 +330,6 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/org.ifaa.android.manager/etc/permissions/org.ifaa.android.manager.permissions.xml:/$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.ifaa.android.manager.permissions.xml
-
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
