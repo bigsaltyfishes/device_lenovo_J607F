2c372b9bc1146ad608db906c6005d67fdf078997
A Big Salty Fish <1055126159@qq.com>
2022-05-20 11:20:30 +0800

    Revert "J607F: Build face hals from source"
    This reverts commit 74cf35d34754d7f21397e3b56d61e359913d4cc9.

--- device.mk
+++ device.mk
@@ -60,11 +60,6 @@ PRODUCT_PACKAGES += \
     bootctrl.lito \
     bootctrl.lito.recovery
 
-# Face
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.face@1.0.vendor \
-    android.hardware.biometrics.face@1.0
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
