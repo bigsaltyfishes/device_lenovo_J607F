fbad1a496e5af4f183b33cb7ea69a7cda87e19b7
A Big Salty Fish <1055126159@qq.com>
2022-05-22 18:59:04 +0800

    J607F: Switch to Lawnchair if it is exist in source

--- evolution_J607F.mk
+++ evolution_J607F.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/lenovo/J607F/device.mk)
 # Inherit some common Evolution X stuff.
 $(call inherit-product, vendor/evolution/config/common_full_tablet_wifionly.mk)
 
+# Switch to Lawnchair if exist
+$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
+
 DEVICE_MAINTAINER := bigsaltyfishes
 
 # Enable Face Unlock
