ce31500808cffb7dfe97dcc8dcf21f2b6a55af7d
aswin7469 <aswinas@pixysos.com>
2022-06-01 08:13:46 +0000

    J607F: Cleanup aosp Codec2 modules
    * We switched to  QTI Codec2 HAL
    * Also build 64 bit version of codec2 libs
    
    Signed-off-by: bigsaltyfishes <bigsaltyfishes@gmail.com>

--- device.mk
+++ device.mk
@@ -381,10 +381,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
-    libavservices_minijail_vendor \
-    libmm-omxcore \
-    android.hardware.media.c2@1.0.vendor \
-    android.hardware.media.c2@1.2.vendor
+    libavservices_minijail_vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
@@ -412,60 +409,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
-
-# Enable Codec 2.0
-PRODUCT_PACKAGES += \
-    libqcodec2_base \
-    libqcodec2_utils \
-    libqcodec2_platform \
-    libqcodec2_core \
-    libqcodec2_basecodec \
-    libqcodec2_v4l2codec \
-    vendor.qti.media.c2@1.0-service \
-    codec2.vendor.ext-arm64.policy \
-    codec2.vendor.base-arm64.policy
-
-# Codec2 modules
-PRODUCT_PACKAGES += \
-    com.android.media.swcodec \
-    libsfplugin_ccodec
-
-# Media codecs C2
-PRODUCT_PACKAGES += \
-    codec2play \
-    libcodec2_soft_aacdec \
-    libcodec2_soft_aacenc \
-    libcodec2_soft_amrnbdec \
-    libcodec2_soft_amrwbdec \
-    libcodec2_soft_amrnbenc \
-    libcodec2_soft_amrwbenc \
-    libcodec2_soft_av1dec_aom \
-    libcodec2_soft_avcdec \
-    libcodec2_soft_avcenc \
-    libcodec2_soft_common \
-    libcodec2_soft_flacdec \
-    libcodec2_soft_flacenc \
-    libcodec2_soft_g711alawdec \
-    libcodec2_soft_g711mlawdec \
-    libcodec2_soft_av1dec_gav1 \
-    libcodec2_soft_gsmdec \
-    libcodec2_soft_hevcdec \
-    libcodec2_soft_hevcenc \
-    libcodec2_soft_mp3dec \
-    libcodec2_soft_mpeg2dec \
-    libcodec2_soft_mpeg4dec \
-    libcodec2_soft_h263dec \
-    libcodec2_soft_mpeg4enc \
-    libcodec2_soft_h263enc \
-    libcodec2_soft_opusdec \
-    libcodec2_soft_opusenc \
-    libcodec2_soft_rawdec \
-    libcodec2_soft_vorbisdec \
-    libcodec2_soft_vp9dec \
-    libcodec2_soft_vp8dec \
-    libcodec2_soft_vp9enc \
-    libcodec2_soft_vp8enc \
-    libcodec2_soft_xaacdec
 
 # Native Public Libraries
 PRODUCT_COPY_FILES += \
