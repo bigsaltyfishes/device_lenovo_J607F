64c1a2c3adcaafc6aeba240b8228dc0220aeedb9
Chenyang Zhong <zhongcy95@gmail.com>
2022-07-27 19:00:14 +0800

    J607F: Turn camera postproc's thread pool shrink into nop
    Patch the camera postproc impl blob and turn the thread pool shrink function call into a nop.
    
    objdump before:
    6534:	94000a13 	bl	8d80 <_ZN7android8hardware22configureRpcThreadpoolEmb@plt>
    6538:	f94016ad 	ldr	x13, [x21,#40
    
    objdump after:
    6534:	d503201f 	nop
    6538:	f94016ad 	ldr	x13, [x21,#40
    
    Abort message without the patch:
    F DEBUG   : Abort message: 'Binder threadpool cannot be shrunk after starting'
    ...
    F DEBUG   : backtrace:
    ...
    F DEBUG   :       #05 pc 0000000000006534  /vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.bitra.so (RegisterIPostProcService() (.cfi)+636) (BuildId: 38b46953f69c3121f44a14498298c0ec)
    ...
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: A Big Salty Fish <1055126159@qq.com>

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,18 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/130A0094AD1640F9/1F2003D5AD1640F9/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
+            mv "${TMPDIR}/${1##*/}" "${2}"
+            ;;
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.bitra.so)
+            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/130A0094AD1640F9/1F2003D5AD1640F9/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
+            mv "${TMPDIR}/${1##*/}" "${2}"
+            ;;
+    esac
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
