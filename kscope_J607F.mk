418566850e325dfc7a303b241d0ef2219883ddbe
A Big Salty Fish <bigsaltyfishes@gmail.com>
2022-07-31 19:49:06 +0800

    J607F: Fix Laucnher crash

--- kscope_J607F.mk
+++ kscope_J607F.mk
@@ -6,12 +6,12 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from J607F device
 $(call inherit-product, device/lenovo/J607F/device.mk)
 
-# Inherit some common Evolution X stuff.
+# Inherit some common KaleidoscopeOS stuff.
 $(call inherit-product, vendor/kscope/target/product/tablet_no_telephony.mk)
 
 # Switch to Lawnchair if exist
